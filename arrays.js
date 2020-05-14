e8bd5b46a7bbf0ea3b229341061e3aeda019bd02
Sahej Singh <sahejdeep@hotmail.com>
2020-05-14 03:23:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,5 +23,5 @@ return array}
 function destructivelyRemoveElementFromEndOfArray(array) {array.pop()
 return array}
 
-function removeElementFromEndOfArray(array) {array.slice(0, array.length -1)
+function removeElementFromEndOfArray(array) {array = array.slice(0, array.length -1)
 return array}
