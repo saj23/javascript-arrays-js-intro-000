4a4e663a051ea6ece7ab81bd102f9132b07deb12
Sahej Singh <sahejdeep@hotmail.com>
2020-05-14 03:08:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,5 @@ function accessElementInArray(array, index) {return array[index]}
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {array.shift()
 return array}
+
+function removeElementFromBeginningOfArray(array) {array.shift()}
