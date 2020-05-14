6f5cdaa5643097b7b24276e07de3f5779907e024
Sahej Singh <sahejdeep@hotmail.com>
2020-05-14 02:59:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
-return [element, ...array];
+return element, ...array;
 }
+
