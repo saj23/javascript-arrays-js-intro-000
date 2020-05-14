cf64d0571e1fc289ce90f38bc91e011f42366d86
Sahej Singh <sahejdeep@hotmail.com>
2020-05-14 03:06:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,3 +11,5 @@ function addElementToEndOfArray(array, element) {return [...array, element]}
 
 function destructivelyAddElementToEndOfArray(array, element) {array.push(element)
 return array}
+
+function accessElementInArray(array, index) {return array[index]}
